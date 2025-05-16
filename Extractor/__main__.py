faeea0aa0291a218fa5b85fcee2d1aef84fab96d
cpbhai <cp2072782@gmail.com>
2025-05-17 00:42:07 +0530

    Update __main__.py

--- Extractor/__main__.py
+++ Extractor/__main__.py
@@ -1,20 +1,25 @@
 import asyncio
 import importlib
 import signal
-from pyrogram import idle
+from pyrogram import Client
 from Extractor.modules import ALL_MODULES
 
 loop = asyncio.get_event_loop()
+shutdown_event = asyncio.Event()
 
 async def sumit_boot():
     for all_module in ALL_MODULES:
         importlib.import_module("Extractor.modules." + all_module)
 
     print("» ʙᴏᴛ ᴅᴇᴘʟᴏʏ sᴜᴄᴄᴇssғᴜʟʟʏ ✨ 🎉")
-    await idle()
+
+    # Wait until shutdown_event is set
+    await shutdown_event.wait()
+
     print("» ɢᴏᴏᴅ ʙʏᴇ ! sᴛᴏᴘᴘɪɴɢ ʙᴏᴛ.")
 
 def handle_sigterm():
+    shutdown_event.set()  # Triggers the wait to finish
     for task in asyncio.all_tasks():
         task.cancel()
 
