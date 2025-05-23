8c7c607cf9866d2cff1ab34fb39a88fe38fc0c49
cpbhai <cp2072782@gmail.com>
2025-05-23 07:09:43 +0530

    Update __main__.py

--- Extractor/__main__.py
+++ Extractor/__main__.py
@@ -1,23 +1,10 @@
-import importlib
-import os
 from pyrogram import Client
-from Extractor.modules import ALL_MODULES
+import os
 
 API_ID = int(os.environ.get("API_ID", ""))
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-# Import all modules
-for all_module in ALL_MODULES:
-    importlib.import_module("Extractor.modules." + all_module)
-
-print("» ʙᴏᴛ ᴅᴇᴘʟᴏʏ sᴜᴄᴄᴇssғᴜʟʟʏ ✨ 🎉")
-
 if __name__ == "__main__":
-    app = Client(
-        "main_bot",
-        api_id=API_ID,
-        api_hash=API_HASH,
-        bot_token=BOT_TOKEN,
-    )
+    app = Client("test", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
     app.run()
